8875c28939ba53d021ab21dd475cdc641da7c715
SAD1011 <77442071+SAD1011@users.noreply.github.com>
2021-09-16 14:55:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var Canvas = new fabric.Canvas('MyCanvas');
+var x = document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        Canvas.add(block_image_object);
+    });
+}
+function playSound()
+{
+    x.play();
+}
